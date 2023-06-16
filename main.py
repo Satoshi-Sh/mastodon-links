798e540854679f78e485f8f5bdba1c424ce121cf
satoshi-sh <satoss1108@gmail.com>
2023-06-16 15:40:29 -0500

    Modified main.py to implement stremlit page

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from mastodon import Mastodon;
 from dotenv import dotenv_values;
 env_vars = dotenv_values('.env')
 
-def main():
+def get(token,url):
     mastodon = Mastodon(
-        access_token=env_vars['ACCESS_TOKEN'],
-        api_base_url=env_vars['API_URL'],
+        access_token=token,
+        api_base_url=url,
     )
 
     account_id = mastodon.account_verify_credentials()['id']
@@ -24,7 +24,7 @@ def main():
                 links.append({"title":field.name,"link":field.value.split('"')[1]})
         info['links']=links
         data.append(info)
-    filename = "data/following_url.json"
+    filename = "sample_data/following_url.json"
 
     # Open the file in write mode
     with open(filename, "w") as file:
@@ -32,6 +32,10 @@ def main():
         json.dump(data, file)
 
     print("JSON file created: ", filename)
+
+
+def main():
+    get(env_vars['ACCESS_TOKEN'],env_vars['API_URL'])
 
 if __name__== '__main__':
     main()
